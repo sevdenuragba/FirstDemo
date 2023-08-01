43677aca86c95eb4fbaa41d367f4f727bbb21333
Sevde Nur <sevdenuragba@gmail.com>
2023-08-01 16:16:00 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 219, 9, 62),
+                Color.fromARGB(255, 187, 9, 148),
+                Color.fromARGB(255, 208, 63, 236)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "HEY\nHEY\nHEY",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Color.fromARGB(255, 10, 10, 93),
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
